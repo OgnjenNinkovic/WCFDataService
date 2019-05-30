bc322c3dccc328850b09e6c9ca6f673592f940f7
Ognjen Ninkovic <oninkovic5@gmail.com>
2019-05-31 01:13:40 +0200

    creating wcf data service project

---
+++ BasicWCFDataService/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+using System.Data.Services.Client;
+using BasicWCFDataService.NorthwindWCFDataService;
+namespace BasicWCFDataService
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        NorthwindEntities context = null;
+        Customer customer = null;
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            context = new NorthwindEntities(new Uri("http://localhost:47163/NorthwindWCFDataService.svc/"));
+            var customersQuery =
+                from cust in context.Customers
+                select cust;
+            customersListBox.ItemsSource = customersQuery.ToList();
+        }
+
+        private void customersListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            customer = (Customer)customersListBox.SelectedItem;
+            customersTextBlock.Text = string.Format(
+                "This is customer {0}, in {1}, {2}", customer.CustomerID, customer.City, customer.Country);
+        }
+
+       
+    }
+}
